10552f25f0d932bfa1cf64b7e5bc9655703d925a
Maria Fernanda Mendoza <mendozamafer2000@mesoamericana.edu.gt>
2020-10-12 02:44:08 -0600

    Se separo el disenio base se agrego css se ve en detalle una publicacion

--- blog/urls.py
+++ blog/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.publicacion_lista, name='publicacion_lista'),
+    path('publicacion/<int:pk>/', views.publicacion_detalle, name='detalle_publicacion'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Publicacion
 
@@ -6,3 +6,7 @@ from .models import Publicacion
 def publicacion_lista(request):
     publicaciones = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'blog/publicacion_lista.html', {'publicaciones':publicaciones})
+
+def publicacion_detalle(request,pk):
+    publicacion = get_object_or_404(Publicacion, pk=pk)
+    return render(request, 'blog/publicacion_detalle.html', {'publicacion': publicacion})
