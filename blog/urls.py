0e1e082b47ee71d33597769ec4aa483c1e751395
Maria Fernanda Mendoza <mendozamafer2000@mesoamericana.edu.gt>
2020-10-13 00:32:36 -0600

    Formulario para editar y crear publicaciones

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.publicacion_lista, name='publicacion_lista'),
     path('publicacion/<int:pk>/', views.publicacion_detalle, name='detalle_publicacion'),
+    path('publicacion/nueva', views.publicacion_nueva, name='publicacion_nueva'),
+    path('publicacion/<int:pk>/editar/', views.publicacion_editar, name='publicacion_editar'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Publicacion
-
+from .forms import PublicacionForm
 # Create your views here.
 def publicacion_lista(request):
     publicaciones = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
@@ -10,3 +10,31 @@ def publicacion_lista(request):
 def publicacion_detalle(request,pk):
     publicacion = get_object_or_404(Publicacion, pk=pk)
     return render(request, 'blog/publicacion_detalle.html', {'publicacion': publicacion})
+
+def publicacion_nueva(request):
+    if request.method == "POST":
+        formulario = PublicacionForm(request.POST)
+        if formulario.is_valid():
+            publicacion = formulario.save(commit=False)
+            publicacion.author = request.user
+            publicacion.fecha_publicacion = timezone.now()
+            publicacion.save()
+            return redirect('detalle_publicacion', pk=publicacion.pk)
+    else:
+        formulario= PublicacionForm()
+    return render(request, 'blog/publicacion_editar.html',{'formulario': formulario})
+
+
+def publicacion_editar(request, pk):
+    publicacion = get_object_or_404(Publicacion, pk=pk)
+    if request.method == "POST":
+        formulario = PublicacionForm(request.POST, instance=publicacion)
+        if formulario.is_valid():
+            publicacion = formulario.save(commit=False)
+            publicacion.author = request.user
+            publicacion.fecha_publicacion = timezone.now()
+            publicacion.save()
+            return redirect('detalle_publicacion', pk=publicacion.pk)
+    else:
+        formulario = PublicacionForm(instance=publicacion)
+    return render(request, 'blog/publicacion_editar.html', {'formulario': formulario})
